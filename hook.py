08a3d6d160c34284f43e3a86559f4ece6c4b354a
alemi <ale.migliardi@gmail.com>
2021-04-23 20:58:59 +0200

    Added decorator for error logging

--- hook.py
+++ hook.py
@@ -9,6 +9,7 @@ from .driver import DRIVER
 logger = logging.getLogger(__name__)
 
 @alemiBot.on_message(~filters.edited & ~filters.service, group=999999) # happen last and always!
+@DRIVER.log_error_event
 async def log_message_hook(client, message): # TODO handle edits!
 	fname = None
 	if DRIVER.log_media:
@@ -20,16 +21,19 @@ async def log_message_hook(client, message): # TODO handle edits!
 		DRIVER.parse_message_event(message, file_name=fname)
 
 @alemiBot.on_message(~filters.edited & filters.service, group=999999) # happen last and always!
+@DRIVER.log_error_event
 async def log_service_message_hook(client, message): # TODO handle edits!
 	DRIVER.parse_service_event(message)
 
 
 @alemiBot.on_message(filters.edited, group=999999) # happen last and always!
+@DRIVER.log_error_event
 async def log_edit_hook(client, message): # TODO handle edits!
 	if DRIVER.log_messages:
 		DRIVER.parse_edit_event(message)
 
 @alemiBot.on_deleted_messages(group=999999)
+@DRIVER.log_error_event
 async def log_deleted_hook(client, message):
 	if DRIVER.log_messages:
 		DRIVER.parse_deletion_event(message)
