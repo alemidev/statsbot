34638d0b7f7942305fd60afe3612c1661c2e7442
alemi <ale.migliardi@gmail.com>
2021-04-23 12:01:19 +0200

    Glued it together a little, should be able to log

--- driver.py
+++ driver.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
+from pyrogram.types import Message
 
 from bot import alemiBot
 
-class Database:
+from .util.serializer import diff, extract_chat, extract_message, extract_user, extract_delete
+
+class DatabaseDriver:
 	def __init__(self):
 		kwargs = {}
 		host = alemiBot.config.get("database", "host", fallback="localhost")
@@ -13,8 +16,45 @@ class Database:
 		password = alemiBot.config.get("database", "password", fallback=None)
 		if password:
 			kwargs["password"] = password
+		self.log_messages = alemiBot.config("database", "log_messages", fallback=True)
+		self.log_media = alemiBot.config("database", "log_media", fallback=False)
+		self.logged = 0
+		self.deletions = 0
 
 		self.client = MongoClient(host, port, **kwargs)
 		self.db = self.client[alemiBot.config.get("database", "dbname", fallback="alemibot")]
 
-DB = Database()
+	def parse_message_event(self, message:Message, file_name=None):
+		msg = extract_message(message)
+		if file_name:
+			msg["file"] = file_name
+		self.db.messages.insert_one(msg)
+		self.logged += 1
+
+		usr = extract_user(message)
+		usr_id = usr["id"]
+		prev = self.db.users.find_one({"id": usr_id})
+		if prev:
+			usr = diff(prev, usr)
+		if usr: # don't insert if no diff!
+			self.db.users.update_one({"id": usr_id}, usr, upsert=True)
+
+		chat = extract_chat(message)
+		chat_id = chat["id"]
+		prev = self.db.chats.find_one({"id": chat_id})
+		if prev:
+			chat = diff(prev, chat)
+		if chat: # don't insert if no diff!
+			self.db.chats.update_one({"id": chat_id}, chat, upsert=True)
+
+	def parse_deletion_event(self, message:Message):
+		deletion = extract_delete(message)
+		self.db.deletions.insert_one(deletion)
+		self.deletions += 1
+
+		flt = {"id": deletion["id"]}
+		if "chat" in deletion:
+			flt["chat"] = deletion["chat"]
+		self.db.messages.update_one(flt, {"deleted": True})
+
+DRIVER = DatabaseDriver()
--- util/serializer.py
+++ util/serializer.py
@@ -2,6 +2,23 @@ from typing import Union
 
 from pyrogram.types import Message, User, Chat
 
+def diff(old:Union[dict,str,int], new:Union[dict,str,int]):
+	if not isinstance(old, dict):
+		if old != new:
+			return new
+		return None
+	elif not isinstance(new, dict):
+		return new
+	out = {}
+	for key in new:
+		if key not in old:
+			out[key] = new[key]
+		elif old[key] != new[key]:
+			d = diff(old[key], new[key])
+			if d:
+				out[key] = d
+	return out
+
 def extract_message(msg:Message):
 	return {
 		"_" : "Message",
@@ -9,9 +26,9 @@ def extract_message(msg:Message):
 		"from_user" : msg.from_user.id,
 		"chat" : msg.chat.id,
 		"date" : msg.date,
-		"mentioned"	: msg.mentioned,
 		"from_scheduled" : msg.from_scheduled,
 		"text" : msg.text,
+		"edits" : {},
 	}
 
 def extract_user(user:Union[Message,User]):
@@ -37,11 +54,14 @@ def extract_user(user:Union[Message,User]):
 			"support" : user.is_support,
 		},
 		"photo" : {
-            "small_file_id" : user.photo.small_file_id,
-            "small_photo_unique_id" : user.photo.small_photo_unique_id,
-            "big_file_id" : user.photo.big_file_id,
-            "big_photo_unique_id" : user.photo.big_photo_unique_id,
+			"small_file_id" : user.photo.small_file_id,
+			"small_photo_unique_id" : user.photo.small_photo_unique_id,
+			"big_file_id" : user.photo.big_file_id,
+			"big_photo_unique_id" : user.photo.big_photo_unique_id,
 		},
+		"count": {
+			"total" : 1,
+		}
 	}
 
 def extract_chat(chat:Union[Message,Chat]):
@@ -57,5 +77,14 @@ def extract_chat(chat:Union[Message,Chat]):
 			"scam" : chat.is_scam,
 			"fake" : chat.is_fake,
 			"support" : chat.is_support,
-		}
+		},
+		"count" : 1,
+	}
+
+def extract_delete(deletion:Message):
+	return {
+		"_" : "Delete",
+		"id": deletion.message_id,
+		"chat": deletion.chat.id if deletion.chat else None,
+		"date": deletion.date,
 	}
