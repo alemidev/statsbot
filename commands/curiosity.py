6b6c8657501d923566ed9bedc122c70fff97aee1
alemi <ale.migliardi@gmail.com>
2021-04-25 17:48:01 +0200

    Show extra query in result

--- commands/curiosity.py
+++ commands/curiosity.py
@@ -28,15 +28,14 @@ HELP.add_help(["freq", "frequent"], "find frequent words in messages",
 				"last 100 messages. By default, 10 most frequent words are shown, but number of results " +
 				"can be changed with `-r`. By default, only words of `len > 3` will be considered. " +
 				"A minimum word len can be specified with `-min`. Will search in current group or any specified with `-g`. " +
-				"A single user can be specified with `-u` : only messages from that user will count if provided. Change " +
-				"update interval with `-i`. Extra parameters for the db query can be given with `-q`.",
-				args="[-r <n>] [-min <n>] [-g <group>] [-u <user>] [n] [-i <n>]", public=True)
+				"A single user can be specified with `-u` : only messages from that user will count if provided. " +
+				"Extra parameters for the db query can be given with `-q`.",
+				args="[-r <n>] [-min <n>] [-g <group>] [-u <user>] [-q <{q}>] [n]", public=True)
 @alemiBot.on_message(is_allowed & filterCommand(["freq", "frequent"], list(alemiBot.prefixes), options={
 	"results" : ["-r", "-res"],
 	"minlen" : ["-min"],
 	"group" : ["-g", "-group"],
 	"user" : ["-u", "-user"],
-	"interval" : ["-i", "--interval"],
 	"query" : ["-q", "--query"],
 }, flags=["-all"]))
 @report_error(logger)
@@ -45,7 +44,6 @@ async def frequency_cmd(client, message):
 	results = int(message.command["results"]) if "results" in message.command else 10
 	number = int(message.command["cmd"][0]) if "cmd" in message.command else 10000
 	min_len = int(message.command["minlen"]) if "minlen" in message.command else 3
-	update_interval = int(message.command["interval"]) if "interval" in message.command else 10000
 	query = {"text":{"$exists":1}}
 	if "query" in message.command:
 		query = {**query, **json.loads(message.command["query"])}
@@ -64,27 +62,25 @@ async def frequency_cmd(client, message):
 		user = await client.get_users(int(val) if val.isnumeric() else val)
 		query["user"] = user.id
 	logger.info(f"Counting {results} most frequent words in last {number} messages")
-	response = await edit_or_reply(message, f"` → ` Counting word occurrences...")
+	response = await edit_or_reply(message, f"` → ` Querying...")
 	words = []
 	curr = 0
 	for doc in DRIVER.db.messages.find(query).sort("date", DESCENDING).limit(number):
 		if doc["text"]:
 			words += [ w for w in re.sub(r"[^0-9a-zA-Z\s\n]+", "", doc["text"].lower()).split() if len(w) > min_len ]
 			curr += 1
-			if curr % update_interval == 0:
-				await client.send_chat_action(message.chat.id, "playing")
-				await response.edit(f"` → [{curr}/{number}] ` Counting word occurrences...")
 	count = Counter(words).most_common()
 	from_who = f"(from **{get_username(user)}**)" if user else ""
 	where = "**everywhere**"
+	extra = f"`{message.command['query']}`" if "query" in message.command else ""
 	if group:
 		if group.invite_link:
 			where = f"**[{get_channel(group)}]({group.invite_link})**"
 		else:
 			where = f"**{get_channel(group)}**"
-	output = f"`→ ` {where} {from_who}\n` → ` **{results}** most frequent words __(len > {min_len})__ in last **{curr}** messages:\n"
+	output = f"`→ ` {where} {from_who} {extra}\n` → ` **{results}** most frequent words __(len > {min_len})__ in last **{curr}** messages:\n"
 	for i, word in enumerate(count):
 		output += f"`{i:02d}]{'-'*(results-i)}>` `{word[0]}` `({word[1]})`\n"
 		if i >= results:
 			break
-	await response.edit(output, parse_mode="markdown")
+	await edit_or_reply(response, output, parse_mode="markdown")
