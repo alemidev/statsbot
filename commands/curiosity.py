6e99961adc803dbdf23e0d1434062e9c6079158b
alemi <ale.migliardi@gmail.com>
2021-04-26 12:33:15 +0200

    Fix searching in other groups (only for superuser)

--- commands/curiosity.py
+++ commands/curiosity.py
@@ -8,7 +8,7 @@ from pymongo import DESCENDING
 
 from bot import alemiBot
 
-from util.permission import is_allowed, is_superuser
+from util.permission import is_allowed, is_superuser, check_superuser
 from util.message import edit_or_reply
 from util.getters import get_text, get_username, get_channel
 from util.command import filterCommand
@@ -22,15 +22,15 @@ logger = logging.getLogger(__name__)
 
 HELP = HelpCategory("CURIOSITY")
 
-# This doesn't really fit here, will be moved into statistics plugin once I'm done with a proper statistics plugin
 HELP.add_help(["freq", "frequent"], "find frequent words in messages",
 				"find most used words in last messages. If no number is given, will search only " +
 				"last 100 messages. By default, 10 most frequent words are shown, but number of results " +
 				"can be changed with `-r`. By default, only words of `len > 3` will be considered. " +
-				"A minimum word len can be specified with `-min`. Will search in current group or any specified with `-g`. " +
+				"A minimum word len can be specified with `-min`. Will search in current group by default. Perform " +
+				"a db-wide search with flag `-all`, search in a specific group with `-g` (only for superuser). " +
 				"A single user can be specified with `-u` : only messages from that user will count if provided. " +
-				"Extra parameters for the db query can be given with `-q`.",
-				args="[-r <n>] [-min <n>] [-g <group>] [-u <user>] [-q <{q}>] [n]", public=True)
+				"Extra parameters for the db query can be given with `-q`. (only for superuser)",
+				args="[-r <n>] [-min <n>] [-all | -g <group>] [-u <user>] [-q <{q}>] [n]", public=True)
 @alemiBot.on_message(is_allowed & filterCommand(["freq", "frequent"], list(alemiBot.prefixes), options={
 	"results" : ["-r", "-res"],
 	"minlen" : ["-min"],
@@ -45,17 +45,20 @@ async def frequency_cmd(client, message):
 	number = int(message.command["cmd"][0]) if "cmd" in message.command else 10000
 	min_len = int(message.command["minlen"]) if "minlen" in message.command else 3
 	query = {"text":{"$exists":1}}
-	if "query" in message.command:
+	extra_query = False
+	if check_superuser(message) and "query" in message.command:
+		extra_query = True
 		query = {**query, **json.loads(message.command["query"])}
-	group = None
-	if "-all" not in message.command["flags"]:
-		if "group" in message.command:
-			val = message.command["group"]
-			group = await client.get_chat(int(val) if val.isnumeric() else val)
-			query["chat"] = group.id
-		else:
-			group = message.chat
-			query["chat"] = message.chat.id
+	group = message.chat
+	if "-all" in message.command["flags"]:
+		group = None
+	elif check_superuser(message) and "group" in message.command:
+		tgt = int(message.command["group"])	if message.command["group"].isnumeric() \
+			else message.command["group"] 
+		group = await client.get_chat(tgt)
+
+	if group:
+		query["chat"] = group.id
 	user = None
 	if "user" in message.command:
 		val = message.command["user"]
@@ -70,14 +73,12 @@ async def frequency_cmd(client, message):
 			words += [ w for w in re.sub(r"[^0-9a-zA-Z\s\n]+", "", doc["text"].lower()).split() if len(w) > min_len ]
 			curr += 1
 	count = Counter(words).most_common()
+	# BELOW HERE IS VERY SPAGHETTI, TODO make understandable
 	from_who = f"(from **{get_username(user)}**)" if user else ""
+	extra = f"`{query}`" if extra_query else ""
 	where = "**everywhere**"
-	extra = f"`{message.command['query']}`" if "query" in message.command else ""
 	if group:
-		if group.invite_link:
-			where = f"**[{get_channel(group)}]({group.invite_link})**"
-		else:
-			where = f"**{get_channel(group)}**"
+		where = f"**[{get_channel(group)}]({group.invite_link})**" if group.invite_link else f"**{get_channel(group)}**"
 	output = f"`→ ` {where} {from_who} {extra}\n` → ` **{results}** most frequent words __(len > {min_len})__ in last **{curr}** messages:\n"
 	for i, word in enumerate(count):
 		output += f"`{i:02d}]{'-'*(results-i)}>` `{word[0]}` `({word[1]})`\n"
