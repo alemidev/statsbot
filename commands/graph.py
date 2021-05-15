bd9ee73948d1b1307ff8a5fd8dcfd5b54f758a04
alemi <me@alemi.dev>
2021-05-15 02:00:52 +0200

    allow to search per user

--- commands/graph.py
+++ commands/graph.py
@@ -23,6 +23,7 @@ HELP = HelpCategory("GRAPHS")
 @HELP.add(cmd="[<len>]", sudo=False)
 @alemiBot.on_message(is_allowed & filterCommand(["activity"], list(alemiBot.prefixes), options={
 	"group" : ["-g", "--group"],
+	"user" : ["-u", "--user"],
 }, flags=["-all"]))
 @report_error(logger)
 @set_offline
@@ -32,11 +33,13 @@ async def graph_cmd(client, message):
 
 	(WIP!) show messages sent per day in last X days (default 30, cap 90) in current group \
 	(superuser can specify group or search globally).
+	Get graph of messages from a single user with `-u`.
 	Plot will show most recent values to the right (so 0 is the oldest day).
 	"""
 	prog = ProgressChatAction(client, message.chat.id, action="playing")
 	length = int(message.command[0] or 30)
 	target_group = message.chat
+	target_user = None
 	if check_superuser(message):
 		if "group" in message.command:
 			arg = message.command["group"]
@@ -50,6 +53,10 @@ async def graph_cmd(client, message):
 	query = {"date":{"$gte": now - timedelta(length)}}
 	if target_group:
 		query["chat"] = target_group.id
+	if "user" in message.command:
+		u_input = message.command["user"]
+		target_user = await client.get_users(int(u_input) if u_input.isnumeric() else u_input)
+		query["user"] = target_user.id
 
 	vals = np.zeros(length)
 	for msg in DRIVER.db.messages.find(query):
@@ -68,6 +75,7 @@ async def graph_cmd(client, message):
 
 	prog = ProgressChatAction(client, message.chat.id, action="upload_document")
 	loc = "sent --globally--" if not target_group else f"in --{get_username(target_group)}--" if target_group.id != message.chat.id else ""
+	frm = f"from **{get_username(target_user)}**" if target_user else ""
 	await client.send_photo(message.chat.id, buf, reply_to_message_id=message.message_id,
-									caption=f"` → ` Messages per day {loc} - last **{length}** days", progress=prog.tick)
+									caption=f"`→ ` Messages per day {frm} {loc} \n` → ` Last **{length}** days", progress=prog.tick)
 
