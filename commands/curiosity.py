fdb8621f7562357ace420ff833381f5302e4e7ff
alemi <me@alemi.dev>
2021-05-16 20:12:44 +0200

    ...

--- commands/curiosity.py
+++ commands/curiosity.py
@@ -93,7 +93,7 @@ async def frequency_cmd(client, message):
 			curr += 1
 	count = Counter(words).most_common()
 	stars = 5 if len(count) > 5 else 0
-	output = "`→ ` last **{sep(curr)}** messages\n"
+	output = f"`→ ` last **{sep(curr)}** messages\n"
 	for i, word in enumerate(count):
 		output += f"` → ` [**{sep(word[1])}**] `{word[0]}` {'☆'*stars}\n"
 		stars -=1
