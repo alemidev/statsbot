e39b9d7fb56543e756a49483b043ac2e43d7cbdc
alemi <me@alemi.dev>
2021-05-29 21:18:56 +0200

    better error catching

--- util/getters.py
+++ util/getters.py
@@ -7,6 +7,10 @@ def get_doc_username(doc:dict, mention=True) -> str:
 		if mention:
 			return f'<a href="tg://user?id={doc["id"]}">{doc["first_name"]} {doc["last_name"]}</a>'
 		return doc["first_name"] + " " + doc["last_name"]
-	if mention:
-		return f'<a href="tg://user?id={doc["id"]}">{doc["first_name"]}</a>'
-	return doc["first_name"]
+	if "first_name" in doc and doc["first_name"]:
+		if mention:
+			return f'<a href="tg://user?id={doc["id"]}">{doc["first_name"]}</a>'
+		return doc["first_name"]
+	if "id" in doc:
+		return str(doc["id"])
+	return None
