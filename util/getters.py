0a22e4f30017a4d70a838f7fcf9636c319511467
alemi <ale.migliardi@gmail.com>
2021-05-29 20:12:47 +0200

    fix for dict getter

--- util/getters.py
+++ util/getters.py
@@ -1,9 +1,9 @@
 def get_doc_username(doc:dict, mention=True) -> str:
-	if "username" in doc:
+	if "username" in doc and doc["username"]:
 		if mention:
 			return "@" + doc["username"]
 		return doc["username"]
-	if "last_name" in doc:
+	if "last_name" in doc and doc["last_name"]:
 		if mention:
 			return f'<a href="tg://user?id={doc["id"]}>{doc["first_name"]} {doc["last_name"]}</a>'
 		return doc["first_name"] + " " + doc["last_name"]
