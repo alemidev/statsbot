62e92b4fdb9b893ec6a3de17a5d869f0b78006f2
alemi <ale.migliardi@gmail.com>
2021-06-06 14:12:16 +0200

    wtf just like this?

--- commands/scoreboards.py
+++ commands/scoreboards.py
@@ -62,7 +62,7 @@ async def stats_cmd(client, message):
 	await prog.tick()
 	partecipated_chats = len(await DRIVER.db.messages.distinct("chat", {"user":uid}))
 	await prog.tick()
-	scoreboard_all_users = await DRIVER.db.users.find({}, {"_id":0,"id":1,"messages":1}).to_list()
+	scoreboard_all_users = await DRIVER.db.users.find({}, {"_id":0,"id":1,"messages":1}).to_list(None)
 	await prog.tick()
 	scoreboard_all_users = sorted([ (doc["id"], doc["messages"]) for doc in scoreboard_all_users ], key=lambda x: -x[1])
 	position = [x[0] for x in scoreboard_all_users].index(message.from_user.id)
