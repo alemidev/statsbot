eb532c513c225c862d619b5b3a091a5ca3b364ab
alemi <me@alemi.dev>
2021-08-18 03:11:22 +0200

    prefer channel title than username

--- util/getters.py
+++ util/getters.py
@@ -1,6 +1,8 @@
 def get_doc_username(doc:dict, mention=True) -> str:
 	if "username" in doc and doc["username"]:
 		if mention:
+			if "title" in doc and doc["title"]:
+				return f"<a href=https://t.me/{doc['username']}>{doc['title']}</a>"
 			return "@" + doc["username"]
 		return doc["username"]
 	if "last_name" in doc and doc["last_name"]:
@@ -12,6 +14,8 @@ def get_doc_username(doc:dict, mention=True) -> str:
 			return f'<a href="tg://user?id={doc["id"]}">{doc["first_name"]}</a>'
 		return doc["first_name"]
 	if "title" in doc and doc["title"]:
+		if mention and "invite_link" in doc and doc["invite_link"]:
+			return f"<a href={doc['invite_link']}>{doc['title']}</a>"
 		return doc["title"]
 	if "id" in doc:
 		return str(doc["id"])
